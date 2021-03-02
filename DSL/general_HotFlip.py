324c99f8eea18d9421f191834558b412f478c130
Yuhao Zhang <yuhao.zhang@wisc.edu>
2021-03-02 09:55:19 -0800

    clean up

---
+++ DSL/general_HotFlip.py
@@ -0,0 +1,130 @@
+import numpy as np
+
+from DSL.transformation import Transformation
+from utils import Beam
+
+
+class GeneralHotFlipAttack:
+    def __init__(self, perturbation: list, use_random_aug=False):
+        """
+        :param list perturbation: A perturbation space specified in the DSL.
+        For example, [(Sub(), 2), (Del(), 1)] means at most 2 Sub string transformations and at most 1 Del string
+        transformation. Sub and Del are default string transformations (see transformation.py).
+
+        :Classifier Capacity: Gradient
+
+        The idea of writing a perturbation space in a DSL is first proposed and implemented in the following paper:
+        Robustness to Programmable String Transformations via Augmented Abstract Training. Yuhao Zhang, Aws Albarghouthi, Loris D’Antoni.
+        `[pdf] <https://arxiv.org/abs/2002.09579>`__
+        `[code] <https://github.com/ForeverZyh/A3T>`__
+
+        """
+
+        try:
+            for (a, b) in perturbation:
+                assert isinstance(a, Transformation)
+                assert isinstance(b, int)
+        except:
+            raise AttributeError("param perturbation %s is not in the correct form." % str(perturbation))
+
+        self.perturbation = perturbation
+        self.use_random_aug = use_random_aug
+
+    def gen_adv(self, model, x: list, y, top_n: int, get_embed, return_score=False):
+        """
+        Beam search for the perturbation space. The order of beam search is the same in the perturbation DSL.
+        Some adversarial attack tries to rearrange the order of beam search for better performance.
+        TODO: the order of beam search can be rearranged for better performance.
+        :param model: the victim model, which has to support a method get_grad.
+        :param x: a list of input tokens.
+        :param y: the correct label of input x.
+        :param top_n: maximum number of adversarial candidates given the perturbation space.
+        :param get_embed: get_embed(x, ret_len) takes a list of tokens as inputs and output the embedding matrix with
+        shape (ret_len, dim).
+        if ret_len > len(x), then padding is needed.
+        if ret_len < len(x), then truncating is needed. (Currently, we do not need truncate)
+        :param return_score: whether return the score as a list [(sen, score)], default False, i.e., return [sen]
+        :return: a list of adversarial examples
+        """
+
+        try:
+            model.get_grad
+        except:
+            raise AttributeError("The victim model does not support get_grad method.")
+
+        candidate = Candidate(x, 0 if not self.use_random_aug else np.random.random())
+        candidates = Beam(top_n)
+        candidates.add(candidate, candidate.score)
+        for (tran, delta) in self.perturbation:
+            possible_pos = tran.get_pos(x)  # get a list of possible positions
+            perturbed_set = set()  # restore the perturbed candidates to eliminate duplications
+            for _ in range(delta):
+                # keep the old candidates because we will change candidates in the following loop
+                old_candidates = candidates.check_balance()
+                for (candidate, _) in old_candidates:
+                    if candidate not in perturbed_set:
+                        if len(candidate.x) > 0:
+                            if self.use_random_aug:
+                                candidate.try_all_pos(possible_pos, tran, None, None, candidates)
+                            else:
+                                candidate.try_all_pos(possible_pos, tran, model.get_grad(candidate.x, y), get_embed,
+                                                      candidates)
+                        perturbed_set.add(candidate)
+
+        ret = candidates.check_balance()
+        if return_score:
+            return [(x.x, x.score) for (x, _) in ret]
+        else:
+            return [x.x for (x, _) in ret]
+
+
+class Candidate:
+    def __init__(self, x, score, map_ori2x=None):
+        """
+        Init a candidate
+        :param x: input tokens
+        :param score: score of adversarial attack, the larger the better
+        :param map_ori2x: position mapping from the original input to transformed input
+        """
+        self.x = x
+        self.score = score
+        if map_ori2x is None:
+            self.map_ori2x = [_ for _ in range(len(x))]
+        else:
+            self.map_ori2x = map_ori2x
+
+    def __lt__(self, other):
+        return self.x < other.x
+
+    def try_all_pos(self, pos: list, tran: Transformation, gradients, get_embed, candidates: Beam):
+        """
+        Try all possible positions for trans
+        :param pos: possible positions, a list of (start_pos, end_pos)
+        :param tran: the target transformation
+        :param gradients: the gradients tensor with respect to self.x
+        :param get_embed: a function for getting the embedding of a list of tokens
+        :param candidates: a beam of candidates, will be modified by this methods
+        :return: None
+        """
+
+        for (start_pos_ori, end_pos_ori) in pos:
+            if all(self.map_ori2x[i] is not None for i in range(start_pos_ori, end_pos_ori)):
+                start_pos_x = self.map_ori2x[start_pos_ori]
+                # notice that self.map_ori2x[end_pos] can be None, we need to calculate from self.map_ori2x[end_pos - 1]
+                end_pos_x = self.map_ori2x[end_pos_ori - 1] + 1
+
+                for new_x in tran.transformer(self.x, start_pos_x, end_pos_x):
+                    delta_len = len(new_x) - len(self.x)
+                    if get_embed is not None and gradients is not None:
+                        old_embedding = get_embed(self.x[start_pos_x:])
+                        # ret_len specifies the ret length (padding if not enought)
+                        new_embedding = get_embed(new_x[start_pos_x:min(len(new_x), len(self.x))],
+                                                  ret_len=len(self.x) - start_pos_x)
+                        # gradients[start_pos_x:] has shape (len(self.x) - start_pos_x, dim)
+                        new_score = self.score + np.sum(gradients[start_pos_x:] * (new_embedding - old_embedding))
+                    else:  # we use random sampling
+                        new_score = np.random.random()
+                    new_map_ori2x = self.map_ori2x[:start_pos_ori] + [None] * (end_pos_ori - start_pos_ori) + [
+                        p if p is None else p + delta_len for p in self.map_ori2x[end_pos_ori:]]
+                    new_candidate = Candidate(new_x, new_score, new_map_ori2x)
+                    candidates.add(new_candidate, new_score)
---
+++ DSL/unittest.py
@@ -0,0 +1,203 @@
+import numpy as np
+import unittest
+import random
+import itertools
+import copy
+
+from DSL.general_HotFlip import GeneralHotFlipAttack, Candidate
+from DSL.transformation import Sub, Ins, Del
+from utils import Beam
+
+
+class TestModel1:
+    def __init__(self, dim):
+        self.embed = {}
+        self.dim = dim
+
+
+class TestModel:
+    def __init__(self, dim):
+        self.embed = {}
+        self.dim = dim
+
+    def get_grad(self, x, y):
+        self.scan(x)
+        return np.array([self.embed[w] for w in x])
+
+    def get_embed(self, x, ret_len=None):
+        self.scan(x)
+        if ret_len is None:
+            ret_len = len(x)
+        ret = np.zeros((ret_len, self.dim))
+        for (i, w) in enumerate(x):
+            ret[i] = self.embed[w]
+        return ret
+
+    def scan(self, x):
+        for w in x:
+            if w not in self.embed:
+                self.embed[w] = np.random.random(self.dim)
+
+
+class TestGeneralHotFlipAttack(unittest.TestCase):
+    def test_arguments(self):
+        with self.assertRaises(AttributeError):
+            GeneralHotFlipAttack([(1, 3, 4)])
+        attack = GeneralHotFlipAttack([(Sub("data/pddb", True), 1), (Del(), 2), (Ins(), 3)])
+        with self.assertRaises(AttributeError):
+            attack.gen_adv(TestModel1(100), [], 1, 5, None)
+
+    def test_beam(self):
+        beams = [Beam(4), Beam(3), Beam(2)]
+        test_sets = [[("aaa", 1), ("bbb", 0), ("ccc", -3)], [("aaa", 1), ("bbb", -3), ("ccc", 0)],
+                     [("aaa", 1), ("bbb", -3), ("ccc", 0), ("ddd", 2)]]
+        for (candidates, tests) in zip(beams, test_sets):
+            random.shuffle(tests)
+            for a in tests:
+                candidates.add(*a)
+            tests.sort(key=lambda x: -x[1])
+            tests = tests[:candidates.budget]
+            ans = candidates.check_balance()
+            self.assertEqual(len(tests), len(ans))
+            for (a, b) in zip(tests, ans):
+                self.assertTupleEqual(a, b)
+
+    def test_perturbation(self):
+        model = TestModel(100)
+        perturb = [(Sub("data/pddb", True), 3), (Del(), 2), (Ins(), 1)]
+        attack = GeneralHotFlipAttack(perturb)
+        sen = "i see that a cat sits on the floor .".split()
+        sub_pos = perturb[0][0].get_pos(sen)
+        del_pos = perturb[1][0].get_pos(sen)
+        ins_pos = perturb[2][0].get_pos(sen)
+        cnt = 0
+        for sub_cnt in range(perturb[0][1] + 1):
+            for del_cnt in range(perturb[1][1] + 1):
+                for ins_cnt in range(perturb[2][1] + 1):
+                    for a in itertools.permutations(sub_pos, sub_cnt):
+                        for b in itertools.permutations(del_pos, del_cnt):
+                            for c in itertools.permutations(ins_pos, ins_cnt):
+                                a = set(a)
+                                b = set(b)
+                                c = set(c)
+                                if a.isdisjoint(b) and a.isdisjoint(c) and b.isdisjoint(c):
+                                    cnt += 1
+
+        ans = attack.gen_adv(model, sen, 0, 10000, model.get_embed, True)
+        self.assertEqual(cnt, len(ans))
+        for i in range(1, len(ans)):
+            self.assertTrue(ans[i][1] <= ans[i - 1][1])
+
+    def test_multi_choice(self):
+        model = TestModel(100)
+        perturb = [(Sub("data/pddb", False), 3)]
+        attack = GeneralHotFlipAttack(perturb)
+        sen = "i see that a cat sits on the floor .".split()
+        sub_pos = perturb[0][0].get_pos(sen)
+        for i, (start_pos, end_pos) in enumerate(sub_pos):
+            cnt = 0
+            for _ in perturb[0][0].transformer(sen, start_pos, end_pos):
+                cnt += 1
+            sub_pos[i] = cnt
+
+        cnt = 0
+        for sub_cnt in range(perturb[0][1] + 1):
+            for a in itertools.permutations(sub_pos, sub_cnt):
+                cnt += np.prod(list(a))
+
+        ans = attack.gen_adv(model, sen, 0, 10000, model.get_embed, True)
+        self.assertEqual(cnt, len(ans))
+        for i in range(1, len(ans)):
+            self.assertTrue(ans[i][1] <= ans[i - 1][1])
+
+    def test_single_transformation(self):
+        model = TestModel(100)
+        perturb1 = [(Sub("data/pddb", True), 2)]
+        perturb2 = [(Del(), 2)]
+        perturb3 = [(Ins(), 2)]
+        attack1 = GeneralHotFlipAttack(perturb1)
+        attack2 = GeneralHotFlipAttack(perturb2)
+        attack3 = GeneralHotFlipAttack(perturb3)
+        sen = "i see that a cat sits on the floor .".split()
+        sub_pos = perturb1[0][0].get_pos(sen)
+        exp_ans1 = []
+        for sub_cnt in range(perturb1[0][1] + 1):
+            for a in itertools.permutations(sub_pos, sub_cnt):
+                new_x = sen
+                for (start_pos, end_pos) in a:
+                    new_x = next(perturb1[0][0].transformer(new_x, start_pos, end_pos))
+                exp_ans1.append(new_x)
+
+        del_pos = perturb2[0][0].get_pos(sen)
+        exp_ans2 = []
+        for del_cnt in range(perturb2[0][1] + 1):
+            for a in itertools.permutations(del_pos, del_cnt):
+                new_x = []
+                for (i, w) in enumerate(sen):
+                    if (i, i + 1) not in a:
+                        new_x.append(w)
+
+                exp_ans2.append(new_x)
+
+        ins_pos = perturb3[0][0].get_pos(sen)
+        exp_ans3 = []
+        for ins_cnt in range(perturb3[0][1] + 1):
+            for a in itertools.permutations(ins_pos, ins_cnt):
+                new_x = []
+                for (i, w) in enumerate(sen):
+                    if (i, i + 1) not in a:
+                        new_x.append(w)
+                    else:
+                        new_x.append(w)
+                        new_x.append(w)
+
+                exp_ans3.append(new_x)
+
+        ans1 = attack1.gen_adv(model, sen, 0, 10000, model.get_embed)
+        ans2 = attack2.gen_adv(model, sen, 0, 10000, model.get_embed)
+        ans3 = attack3.gen_adv(model, sen, 0, 10000, model.get_embed)
+
+        def list2map(l):
+            ret = {}
+            for x in l:
+                x = tuple(x)
+                ret[x] = ret.setdefault(x, 0) + 1
+            return ret
+
+        self.assertDictEqual(list2map(exp_ans1), list2map(ans1))
+        self.assertDictEqual(list2map(exp_ans2), list2map(ans2))
+        self.assertDictEqual(list2map(exp_ans3), list2map(ans3))
+
+    def test_map_ori2x(self):
+        model = TestModel(100)
+        perturb = [(Sub("data/pddb", True), 3), (Del({"floor"}), 2), (Ins(), 1)]
+        sen = "i see that a cat sits on the floor .".split()
+        sub_pos = perturb[0][0].get_pos(sen)
+        del_pos = perturb[1][0].get_pos(sen)
+        ins_pos = perturb[2][0].get_pos(sen)
+        candidate = Candidate(["dummy"], 0, [None] * len(sen))
+        candidates = Beam(1)
+        candidate.try_all_pos(ins_pos, perturb[2][0], model.get_grad(candidate.x, 0), model.get_embed,
+                              candidates)
+        self.assertEqual(0, len(candidates.queue))
+        map_ori2x = [None] * len(sen)
+        map_ori2x[sen.index("floor")] = 1
+        candidate = Candidate(["dummy1", "floor", "dummy2"], 0, map_ori2x)  # all perturbation can be applied
+        candidates = Beam(1)
+        candidate.try_all_pos(sub_pos, perturb[0][0], model.get_grad(candidate.x, 0), model.get_embed,
+                              candidates)
+        self.assertSequenceEqual(["dummy1", "flooring", "dummy2"], candidates.check_balance()[0][0].x)
+
+        candidates = Beam(1)
+        candidate.try_all_pos(del_pos, perturb[1][0], model.get_grad(candidate.x, 0), model.get_embed,
+                              candidates)
+        self.assertSequenceEqual(["dummy1", "dummy2"], candidates.check_balance()[0][0].x)
+
+        candidates = Beam(1)
+        candidate.try_all_pos(ins_pos, perturb[2][0], model.get_grad(candidate.x, 0), model.get_embed,
+                              candidates)
+        self.assertSequenceEqual(["dummy1", "floor", "floor", "dummy2"], candidates.check_balance()[0][0].x)
+
+
+if __name__ == '__main__':
+    unittest.main()
